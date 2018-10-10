406e30a5c016e2a87c8691bb681927d0581c15f6
Arie van Boxel <develop@arievanboxel.nl>
2018-10-10 18:22:34 +0200

    Persist by using Codable

--- Todo/Controllers/TodoListViewController.swift
+++ Todo/Controllers/TodoListViewController.swift
@@ -13,26 +13,12 @@ class TodoListViewController: UITableViewController {
     var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
-    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let newItem1 = Item()
-        newItem1.title = "Een"
-        itemArray.append(newItem1)
-        
-        let newItem2 = Item()
-        newItem2.title = "Twee"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Drie"
-        itemArray.append(newItem3)
-        
-        guard let savedArray = defaults.array(forKey: "TodoListArray") as? [Item] else { return }
 
-        itemArray = savedArray
-
+        loadItems()
     }
 
     // MARK: - TableView Datasource methods
@@ -53,7 +39,7 @@ class TodoListViewController: UITableViewController {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        tableView.cellForRow(at: indexPath)?.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
+        saveItems()
         
         // Hide gray selection
         tableView.deselectRow(at: indexPath, animated: true)
@@ -73,9 +59,7 @@ class TodoListViewController: UITableViewController {
                 
                 self.itemArray.append(newItem)
                 
-                self.defaults.set(self.itemArray, forKey: "TodoListArray")
-                
-                self.tableView.reloadData()
+                self.saveItems()
             }
         }
         alert.addTextField { (alertTextField) in
@@ -85,6 +69,35 @@ class TodoListViewController: UITableViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - Store and read
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        self.tableView.reloadData()
+
+    }
+    
+    func loadItems() {
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
