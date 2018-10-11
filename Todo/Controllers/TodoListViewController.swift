a87050748ae9620a041022832ac2e06418db957d
Arie van Boxel <develop@arievanboxel.nl>
2018-10-11 10:55:13 +0200

    Before implementing relationships

--- Todo/Controllers/TodoListViewController.swift
+++ Todo/Controllers/TodoListViewController.swift
@@ -14,10 +14,13 @@ class TodoListViewController: UITableViewController {
     var itemArray = [Item]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
 //        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        searchBar.delegate = self
         loadItems()
     }
 
@@ -36,7 +39,7 @@ class TodoListViewController: UITableViewController {
     
     // MARK: - TableView Delegate methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        // Delete example:
 //        context.delete(itemArray[indexPath.row])
 //        itemArray.remove(at: indexPath.row)
          itemArray[indexPath.row].done = !itemArray[indexPath.row].done
@@ -86,15 +89,43 @@ class TodoListViewController: UITableViewController {
 
     }
     
-    func loadItems() {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
 
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
         do {
             itemArray = try context.fetch(request)
         } catch {
             print(error.localizedDescription)
         }
-        
+        tableView.reloadData()
     }
+    
+}
+
+// MARK: - SearchBarDelegate
+extension TodoListViewController:UISearchBarDelegate {
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // Query the database
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        // Filter
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        // Sort
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            // Hide keyboard and flickering cursor
+            // (Be shure to be inside the main thread)
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+    }
+
 }
 
